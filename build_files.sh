502030cfaac2a02f35cf335b2a4954ed89d77ba9
Abdelmouneim <monuim378@gmail.com>
2026-05-19 22:48:58 +0100

    Fix build_files permissions and static build

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 echo " Building Project... "
-
-# التثبيت مباشرة عبر uv مع علم التوافقية والأمان الكامل
-uv pip install -r requirements.txt --system --break-system-packages
-
-# تشغيل أمر الـ collectstatic المباشر
+python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
-
 echo " Build Finished! "

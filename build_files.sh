4a2a6c3d2753e3202787c00abb36110979ead191
Abdelmouneim <monuim378@gmail.com>
2026-05-19 22:15:31 +0100

    Fix line endings and update build script

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo " Building Project... "
-python3 -m pip install -r requirements.txt
+
+# تثبيت الحزم باستخدام أداة uv الذكية المتوافقة مع تحديث Vercel
+uv pip install -r requirements.txt --system
+
+# تشغيل أمر تجميع ملفات الستاتيك الخاص بديانغو
 python3 manage.py collectstatic --noinput --clear
+
 echo " Build Finished! "

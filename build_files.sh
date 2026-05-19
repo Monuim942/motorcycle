ee27291022cde772d4e071dca75f221f47cd3583
Abdelmouneim <monuim378@gmail.com>
2026-05-19 22:28:50 +0100

    Configure STATIC_ROOT for Vercel and update build script flags

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo " Building Project... "
 
-# تثبيت الحزم باستخدام أداة uv الذكية المتوافقة مع تحديث Vercel
-uv pip install -r requirements.txt --system
+# تثبيت الحزم بأمان وتحديث الـ pip
+python3 -m pip install --upgrade pip
+uv pip install -r requirements.txt --system --break-system-packages
 
-# تشغيل أمر تجميع ملفات الستاتيك الخاص بديانغو
+# تجميع الملفات الثابتة
 python3 manage.py collectstatic --noinput --clear
 
 echo " Build Finished! "
--- motorcycle/settings.py
+++ motorcycle/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
